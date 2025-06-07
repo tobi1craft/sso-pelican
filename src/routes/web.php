f9a1dbcc49fd29420f9e3108481e111afe1367f7
tobi1craft <tobi1craft.mc@gmail.com>
2025-06-07 15:54:10 +0000

    remove leftover

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use Tobi1craft\Sso\Http\Controllers\SsoController;
 
 Route::middleware(['web'])->group(function (): void {
-    Route::get('/request-token', [SsoController::class, 'requestToken']);
     Route::get('/request-sso', [SsoController::class, 'requestLogin']);
     Route::get('/sso/{token}', [SsoController::class, 'handle'])->name('sso-tobi1craft.login');
 });
